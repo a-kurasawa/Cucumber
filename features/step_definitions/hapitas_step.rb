52812b6130b9c155bb907aaad378e2f5242030f1
a-kurasawa <a-kurasawa@oz-vision.co.jp>
2015-12-27 00:46:17 +0900

    'sampleは不要なので削除'

--- features/step_definitions/hapitas_step.rb
+++ features/step_definitions/hapitas_step.rb
@@ -1,3 +1,7 @@
 When(/^PC版ハピタスのTOPページにアクセスすると$/) do
   visit 'http://hapitas.jp'
 end
+
+Then(/^メニューがある$/) do
+  page.find('#openswitch').should have_content('メニュー')
+end
--- features/step_definitions/sample_steps.rb
+++
@@ -1,8 +0,0 @@
-When(/^PC版ハピタスにアクセスすると$/) do
-  visit 'http://hapitas.jp'
-end
-
-Then(/^メニューがある$/) do
-  page.find('#openswitch').should have_content('メニュー')
-end
-
